b27db16e6924e0bea7ce55dd7bc46ab61592ab9d
luca590 <lucasgaylord@protonmail.com>
2018-02-17 06:01:48 +0800

    router setup

---
+++ server_code/lib/router.ex
@@ -0,0 +1,13 @@
+defmodule PlugEx.Router do
+  use Plug.Router
+
+  plug :match
+  plug :dispatch
+
+  get "/" do
+    send_resp(conn, 200, "Successful test!")
+  end
+
+  match _, do: send_resp(conn, 404, "404 - site not setup for this page")
+
+end 
